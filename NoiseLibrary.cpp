3dabf89bff353f7125de31bed5ea56aa9b9e7c20
Manfred Hästmark <hastmark@student.chalmers.se>
2023-06-04 20:38:28 +0200

    Added 2D implementation of simplex noise

--- NoiseLibrary.cpp
+++ NoiseLibrary.cpp
@@ -9,7 +9,99 @@ namespace noise
 #pragma region Simplex
 
 	float Simplex::evaluate(float x) { return 0; }
-	float Simplex::evaluate(float x, float y) { return 0; }
+
+	float Simplex::evaluate(float x, float y) 
+    {
+        float n0, n1, n2;   // Noise contributions from the three corners
+
+        // Skewing/Unskewing factors for 2D
+        const float F2 = 0.366025403f;  // F2 = (sqrt(3) - 1) / 2
+        const float G2 = 0.211324865f;  // G2 = (3 - sqrt(3)) / 6   = F2 / (1 + 2 * K)
+
+        // Skew the input space to determine which simplex cell we're in
+        float s = (x + y) * F2;  // Hairy factor for 2D
+        float xs = x + s;
+        float ys = y + s;
+        int i = fast_floor(xs);
+        int j = fast_floor(ys);
+
+        // Unskew the cell origin back to (x,y) space
+        float t = (i + j) * G2;
+        float X0 = i - t;
+        float Y0 = j - t;
+        float x0 = x - X0;  // The x,y distances from the cell origin
+        float y0 = y - Y0;
+
+        // For the 2D case, the simplex shape is an equilateral triangle.
+        // Determine which simplex we are in.
+        int i1, j1;  // Offsets for second (middle) corner of simplex in (i,j) coords
+        if (x0 > y0)
+        {   // lower triangle, XY order: (0,0)->(1,0)->(1,1)
+            i1 = 1;
+            j1 = 0;
+        }
+        else
+        {   // upper triangle, YX order: (0,0)->(0,1)->(1,1)
+            i1 = 0;
+            j1 = 1;
+        }
+
+        // A step of (1,0) in (i,j) means a step of (1-c,-c) in (x,y), and
+        // a step of (0,1) in (i,j) means a step of (-c,1-c) in (x,y), where
+        // c = (3-sqrt(3))/6
+
+        float x1 = x0 - i1 + G2;            // Offsets for middle corner in (x,y) unskewed coords
+        float y1 = y0 - j1 + G2;
+        float x2 = x0 - 1.0f + 2.0f * G2;   // Offsets for last corner in (x,y) unskewed coords
+        float y2 = y0 - 1.0f + 2.0f * G2;
+
+        // Work out the hashed gradient indices of the three simplex corners
+        int ii = i & 255;
+        int jj = j & 255;
+        int gi0 = p[ii + p[jj]] & 255;
+        int gi1 = p[ii + i1 + p[jj + j1]] & 255;
+        int gi2 = p[ii + 1 + p[jj + 1]] & 255;
+
+        // Calculate the contribution from the first corner
+        float t0 = 0.5f - x0 * x0 - y0 * y0;
+        if (t0 < 0.0f)
+        {
+            n0 = 0.0f;
+        }
+        else
+        {
+            t0 *= t0;
+            n0 = t0 * t0 * grad(gi0, x0, y0);
+        }
+
+        // Calculate the contribution from the second corner
+        float t1 = 0.5f - x1 * x1 - y1 * y1;
+        if (t1 < 0.0f)
+        {
+            n1 = 0.0f;
+        }
+        else
+        {
+            t1 *= t1;
+            n1 = t1 * t1 * grad(gi1, x1, y1);
+        }
+
+        // Calculate the contribution from the third corner
+        float t2 = 0.5f - x2 * x2 - y2 * y2;
+        if (t2 < 0.0f)
+        {
+            n2 = 0.0f;
+        }
+        else
+        {
+            t2 *= t2;
+            n2 = t2 * t2 * grad(gi2, x2, y2);
+        }
+
+        // Add contributions from each corner to get the final noise value.
+        // The result is scaled to return values in the interval [-1,1].
+        return 45.23065f * (n0 + n1 + n2);
+    }
 
 	float Simplex::evaluate(float x, float y, float z) 
 	{
